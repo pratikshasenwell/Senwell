d0b1406f210e2d36e2b4a20f52596241568fb782
pratiksha <pratiksha.senwell@gmail.com>
2021-06-04 19:33:38 +0530

    no message

--- src/app/components/pages/buisness-development-executive-uk/buisness-development-executive-uk.component.ts
+++ src/app/components/pages/buisness-development-executive-uk/buisness-development-executive-uk.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { AuthServiceService } from './../../../service/auth-service.service'
 
 @Component({
   selector: 'app-buisness-development-executive-uk',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuisnessDevelopmentExecutiveUkComponent implements OnInit {
 
-  constructor() { }
+  ReactCandidate: FormGroup
+
+  constructor(private formbuilder: FormBuilder, private authService: AuthServiceService) {
+    this.ReactCandidate = this.formbuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      company: ['', Validators.required],
+      ctc: ['', Validators.required],
+      experience: ['', Validators.required],
+      file: ['', Validators.required]
+
+    })
+  }
 
   ngOnInit(): void {
+    
+  }
+  uploadCandidateCv(event: any) {
+    // 
+    this.authService.UploadCandidateFile(event)
+  }
+  saveReactcandidate() {
+    debugger
+    // this.ReactCandidate.patchValue({file:this.authService.docDownloadUrl$.value})
+    this.authService.SaveCandidateData({formdata:this.ReactCandidate.value,downloadurl:this.authService.docDownloadUrl$.value})
   }
 
 }
--- src/app/components/pages/php-developer/php-developer.component.ts
+++ src/app/components/pages/php-developer/php-developer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { AuthServiceService } from './../../../service/auth-service.service'
 
 @Component({
   selector: 'app-php-developer',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./php-developer.component.css']
 })
 export class PhpDeveloperComponent implements OnInit {
+  ReactCandidate: FormGroup
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder, private authService: AuthServiceService) {
+    this.ReactCandidate = this.formbuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      company: ['', Validators.required],
+      ctc: ['', Validators.required],
+      experience: ['', Validators.required],
+      file: ['', Validators.required]
+
+    })
+  }
 
   ngOnInit(): void {
+   
+  }
+  uploadCandidateCv(event: any) {
+    // 
+    this.authService.UploadCandidateFile(event)
+  }
+  saveReactcandidate() {
+    debugger
+    // this.ReactCandidate.patchValue({file:this.authService.docDownloadUrl$.value})
+    this.authService.SaveCandidateData({formdata:this.ReactCandidate.value,downloadurl:this.authService.docDownloadUrl$.value})
   }
 
 }
